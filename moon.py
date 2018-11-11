f0498131d6b74f49bb902c18eadd734dbbc6c384
Nicholas Tomlin <Nicholas_Tomlin@brown.edu>
2018-11-10 20:56:44 -0500

    Added graph printing

--- moon.py
+++ moon.py
@@ -15,13 +15,9 @@ from gerrychain.accept import always_accept
 import numpy as np
 from math import sqrt
 import matplotlib.pyplot as plt
+import math
+from termcolor import colored
 
-
-def example_partition():
-    graph = networkx.complete_graph(4)
-    assignment = {0: 1, 1: 1, 2: 2, 3:2}
-    partition = Partition(graph, assignment)
-    return (graph, partition)
 
 def get_political_leanings(size, num_classes=2):
     # return np.random.randint(0, num_classes, size)
@@ -93,6 +89,7 @@ def district_assignment(size, districts):
 
     assignment_matrix = np.reshape(assignment_matrix[:side_1*side_2], (side_1, side_2))
     assignment_matrix[1::2, :] = assignment_matrix[1::2, ::-1]
+    assignment_matrix -= np.ones(size)
 
     assignment_dict = dict()
 
@@ -102,15 +99,29 @@ def district_assignment(size, districts):
 
     return assignment_dict
 
-leanings_grid = get_political_leanings((10,10))
-# print(get_polarity(grid))
+def print_partition(partition, leanings, size):
+    matrix = np.zeros(size)
+    for key in partition.assignment:
+        matrix[key] = partition.assignment[key]
+    output = ""
+    for i, row in enumerate(matrix):
+        for j, value in enumerate(row):
+            leaning = leanings[i, j]
+            if leaning == 0:
+                output += colored(int(value), 'red')
+            else:
+                output += colored(int(value), 'blue')
+        output += "\n"
+    print(output)
+
+
 
 if __name__ == '__main__':
     
     size = (10,10)
-    districts = 7
+    districts = 4
  
-    # grid = Grid(size, assignment = district_assignment(size, districts))
+    # grid = Grid(size, assignment = )
     
     flipped_count = 0
     flipped_value_sum = 0
@@ -119,26 +130,28 @@ if __name__ == '__main__':
     nonflipped_value_sum = 0
     nonflipped_values = []
     lowest_min = .99
+
     for i in range(36):
         leanings_grid = get_political_leanings(size)
         polarity = get_polarity(leanings_grid)
 
-        grid = Grid(size)
+        # print(district_assignment(size, districts))
+        grid = Grid(size, assignment=district_assignment(size, districts))
         # print("Results\n--------")
-        # print(grid)
+        
 
         chain = MarkovChain(
         proposal=propose_random_flip,
         is_valid=Validator([new_constraint]),
         accept=always_accept,
         initial_state=grid,
-        total_steps=100005
+        total_steps=1000005
         )
 
         
         for idx, partition in enumerate(chain):
             if (idx % 1000) == 0:
-                district_votes = get_district_vote(partition.assignment, leanings_grid) # counts
+                district_votes = get_district_vote(partition.assignment, leanings_grid, num_districts=districts) # counts
                 polarities = get_district_polarities(district_votes)
                 # print(delta_dvec(polarities, polarity))
                 votes = get_vote(district_votes) # booleans
@@ -151,6 +164,15 @@ if __name__ == '__main__':
                     flipped_count += 1
                     flipped_value_sum += value
                     flipped_values.append(value)
+                    if (value < lowest_min):
+                        lowest_min = value
+                        print("-----\nNew lowest flipped value:")
+                        print(value)
+                        print_partition(partition, leanings_grid, size)
+                        print("Polarity: {}".format(polarity))
+                        # Get winners:
+                        print("Votes: {}".format(votes))
+                        print(leanings_grid)
                 else:
                     nonflipped_count += 1
                     nonflipped_value_sum += value
@@ -162,7 +184,7 @@ if __name__ == '__main__':
             if (idx % 100000) == 0 and idx > 0:
                 print("-----------")
                 # print("Number samples: {}".format(idx/1000))
-                print("Iteration: {}".format(i))
+                # print("Iteration: {}".format(i))
                 print("Number flipped elections: {}".format(flipped_count))
                 if flipped_count != 0:
                     print("Flipped avg: {}".format(flipped_value_sum/flipped_count))
